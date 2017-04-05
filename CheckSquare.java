3e2a46ac8957be9d996c6925909557cea398fe5c
farr16 <msamfarr@outlook.com>
2017-04-05 15:41:09 -0700

    Add point distance computations
    Adds a test method for computing the distance between one of the points
    and the three others

--- CheckSquare.java
+++ CheckSquare.java
@@ -12,7 +12,6 @@ public class CheckSquare
 {
 	public static void main(String[] args)
 	{
-		
 		double[][] points = getPointsFromInput();
 		
 		// Print the four points to make sure input was captured correctly
@@ -20,12 +19,14 @@ public class CheckSquare
 		for (int i=0; i<points.length; i++) {
 			System.out.println("Point #" + (i+1) + ": (" + points[i][0] + " , " + points[i][1] + ")");
 		}
+		
+		computePointDistances(points);
 	}
 	
 	/**
 	 * getPointsFromInput
 	 *
-	 * Function which gathers user input from the console for the points that will be looked at
+	 * Method which gathers user input from the console for the points that will be looked at
 	 */
 	private static double[][] getPointsFromInput()
 	{
@@ -67,13 +68,34 @@ public class CheckSquare
 		return points;
 	}
 	
-	private static double getDistance(double x1, double y1, double x2, double y2) {
-		
+	/**
+	 * getDistance
+	 *
+	 * Method which computes the distance between two points
+	 */
+	private static double getDistance(double x1, double y1, double x2, double y2) 
+	{	
 		double xDist = x1 - x2;
 		double yDist = y1 - y2;
 		double dist = Math.sqrt(Math.pow(xDist, 2)+ Math.pow(yDist, 2));
 		
 		return dist;
-		
+	}
+	
+	/**
+	 * computePointDistances
+	 *
+	 * Test method which prints out the distances between points
+	 */
+	private static void computePointDistances(double[][] points)
+	{
+		double firstX = points[0][0];
+		double firstY = points[0][1];
+		System.out.print("\n");
+		double distance = 0d;
+		for (int i=1; i<points.length; i++) {
+			distance = getDistance(firstX, firstY, points[i][0], points[i][1]);
+			System.out.println("Distance from point #1 to point #" + (i+1) + " = " + distance );
+		}
 	}
 }
