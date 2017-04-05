25ccab46f2baeb5f27e780b125bae6eadcb28c5b
farr16 <msamfarr@outlook.com>
2017-04-04 18:46:47 -0700

    Add scanner input
    Adds text input using scanner, and echoes that input to the command line
    to test

---
+++ CheckSquare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CheckSquare
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Input point #1 in the format x,y\n");
+		System.out.println(scanner.nextLine());
+		
+	}
+}
