2b201d5bf09b61252afab2ad80a46dd576a1f07b
farr16 <msamfarr@outlook.com>
2017-04-04 22:46:53 -0700

    Add getPointsFromInput method
    Adds method for getting four points from user input

--- CheckSquare.java
+++ CheckSquare.java
@@ -1,41 +1,67 @@
 import java.util.Scanner;
 
+/**
+ * CheckSquare
+ *
+ * @author Matthew Farr
+ *
+ * Application which takes four points from user input and determines if those four points make up a square.
+ */
 public class CheckSquare
 {
 	public static void main(String[] args)
 	{
+		float[][] points = getPointsFromInput();
+		
+		// Print the four points to make sure input was captured correctly
+		for (int i=0; i<points.length; i++) {
+			System.out.println("Point #" + (i+1) + ": (" + points[i][0] + " , " + points[i][1] + ")");
+		}
+		
+	}
+	
+	/**
+	 * getPointsFromInput
+	 *
+	 * Function which gathers user input from the console for the points that will be looked at
+	 */
+	public static float[][] getPointsFromInput()
+	{
 		Scanner scanner = new Scanner(System.in);
 		
-		float[] point = null;
+		float[][] points = new float[4][2];
 		
-		while (point == null) 
+		for (int i=0; i<points.length; i++) 
 		{
-			System.out.print("Input point #1 in the format x,y\n");
+			boolean gettingInputI = true;
 			
-			String input = scanner.nextLine();
-			
-			String[] pair = input.split(",");
-			
-			if (pair.length != 2) 
+			while (gettingInputI)
 			{
-				System.out.println("Please input two numbers separated by a single comma.");
-				continue;
-			}
-			
-			try 
-			{
-				point = new float[2];
-				point[0] = Float.parseFloat(pair[0]);
-				point[1] = Float.parseFloat(pair[1]);
-			}
-			catch (NumberFormatException e) 
-			{
-				System.out.println("Please make sure comma separated values are proper float numbers");
-				point = null;
+				System.out.println("Input point #" + (i+1) + " in the format x,y");
+				
+				String input = scanner.nextLine();
+				
+				String[] pair = input.split(",");
+				
+				if (pair.length != 2)
+				{
+					System.out.println("Please input two numbers separated by a single comma.");
+					continue;
+				}
+				
+				try
+				{
+					points[i][0] = Float.parseFloat(pair[0]);
+					points[i][1] = Float.parseFloat(pair[1]);
+					gettingInputI = false;
+				}
+				catch (NumberFormatException e)
+				{
+					System.out.println("Please make sure comma separated values are proper float numbers");
+				}
 			}
 		}
 		
-		System.out.println("Point x: " + point[0] + "\nPoint y: " + point[1]);
-		
+		return points;
 	}
 }
